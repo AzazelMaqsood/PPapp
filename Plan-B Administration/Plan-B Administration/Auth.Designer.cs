584a81e2f14058738f49ee2dcc772af918c16a1c
Максуд <shpiram@bk.ru>
2018-11-24 15:41:57 +0300

    Created Auth.cs
    Создан дизайн окна авторизации

---
+++ Plan-B Administration/Plan-B Administration/Auth.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Plan_B_Administration
+{
+    partial class Auth
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoginTxtBox = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.PassTxtBox = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.materialCheckBox1 = new MaterialSkin.Controls.MaterialCheckBox();
+            this.materialRaisedButton1 = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.materialDivider1 = new MaterialSkin.Controls.MaterialDivider();
+            this.SuspendLayout();
+            // 
+            // LoginTxtBox
+            // 
+            this.LoginTxtBox.Depth = 0;
+            this.LoginTxtBox.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LoginTxtBox.Hint = "Введите логин";
+            this.LoginTxtBox.Location = new System.Drawing.Point(47, 112);
+            this.LoginTxtBox.MouseState = MaterialSkin.MouseState.HOVER;
+            this.LoginTxtBox.Name = "LoginTxtBox";
+            this.LoginTxtBox.PasswordChar = '\0';
+            this.LoginTxtBox.SelectedText = "";
+            this.LoginTxtBox.SelectionLength = 0;
+            this.LoginTxtBox.SelectionStart = 0;
+            this.LoginTxtBox.Size = new System.Drawing.Size(342, 28);
+            this.LoginTxtBox.TabIndex = 0;
+            this.LoginTxtBox.UseSystemPasswordChar = false;
+            // 
+            // PassTxtBox
+            // 
+            this.PassTxtBox.Depth = 0;
+            this.PassTxtBox.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.PassTxtBox.Hint = "Введите пароль";
+            this.PassTxtBox.Location = new System.Drawing.Point(47, 172);
+            this.PassTxtBox.MouseState = MaterialSkin.MouseState.HOVER;
+            this.PassTxtBox.Name = "PassTxtBox";
+            this.PassTxtBox.PasswordChar = '*';
+            this.PassTxtBox.SelectedText = "";
+            this.PassTxtBox.SelectionLength = 0;
+            this.PassTxtBox.SelectionStart = 0;
+            this.PassTxtBox.Size = new System.Drawing.Size(342, 28);
+            this.PassTxtBox.TabIndex = 1;
+            this.PassTxtBox.UseSystemPasswordChar = false;
+            // 
+            // materialCheckBox1
+            // 
+            this.materialCheckBox1.AutoSize = true;
+            this.materialCheckBox1.Depth = 0;
+            this.materialCheckBox1.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.materialCheckBox1.Location = new System.Drawing.Point(47, 232);
+            this.materialCheckBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.materialCheckBox1.MouseLocation = new System.Drawing.Point(-1, -1);
+            this.materialCheckBox1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialCheckBox1.Name = "materialCheckBox1";
+            this.materialCheckBox1.Ripple = true;
+            this.materialCheckBox1.Size = new System.Drawing.Size(167, 30);
+            this.materialCheckBox1.TabIndex = 2;
+            this.materialCheckBox1.Text = "Запомнить меня";
+            this.materialCheckBox1.UseVisualStyleBackColor = true;
+            // 
+            // materialRaisedButton1
+            // 
+            this.materialRaisedButton1.Depth = 0;
+            this.materialRaisedButton1.Font = new System.Drawing.Font("HelvLight", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.materialRaisedButton1.Location = new System.Drawing.Point(232, 232);
+            this.materialRaisedButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialRaisedButton1.Name = "materialRaisedButton1";
+            this.materialRaisedButton1.Primary = true;
+            this.materialRaisedButton1.Size = new System.Drawing.Size(157, 30);
+            this.materialRaisedButton1.TabIndex = 3;
+            this.materialRaisedButton1.Text = "Войти";
+            this.materialRaisedButton1.UseVisualStyleBackColor = true;
+            // 
+            // materialDivider1
+            // 
+            this.materialDivider1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialDivider1.Depth = 0;
+            this.materialDivider1.Location = new System.Drawing.Point(16, 285);
+            this.materialDivider1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialDivider1.Name = "materialDivider1";
+            this.materialDivider1.Size = new System.Drawing.Size(405, 1);
+            this.materialDivider1.TabIndex = 4;
+            this.materialDivider1.Text = "materialDivider1";
+            // 
+            // Auth
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(431, 307);
+            this.Controls.Add(this.materialDivider1);
+            this.Controls.Add(this.materialRaisedButton1);
+            this.Controls.Add(this.materialCheckBox1);
+            this.Controls.Add(this.PassTxtBox);
+            this.Controls.Add(this.LoginTxtBox);
+            this.Name = "Auth";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Авторизация";
+            this.TransparencyKey = System.Drawing.SystemColors.Info;
+            this.Load += new System.EventHandler(this.Auth_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialSingleLineTextField LoginTxtBox;
+        private MaterialSkin.Controls.MaterialSingleLineTextField PassTxtBox;
+        private MaterialSkin.Controls.MaterialCheckBox materialCheckBox1;
+        private MaterialSkin.Controls.MaterialRaisedButton materialRaisedButton1;
+        private MaterialSkin.Controls.MaterialDivider materialDivider1;
+    }
+}
+
---
+++ Plan-B Administration/Plan-B Administration/Auth.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Plan_B_Administration
+{
+    public partial class Auth : MaterialSkin.Controls.MaterialForm
+    {
+        public Auth()
+        {
+            InitializeComponent();
+            MaterialSkin.MaterialSkinManager SkinManager = MaterialSkin.MaterialSkinManager.Instance;
+            SkinManager.AddFormToManage(this);
+            SkinManager.Theme = MaterialSkin.MaterialSkinManager.Themes.LIGHT;
+            SkinManager.ColorScheme = new MaterialSkin.ColorScheme(MaterialSkin.Primary.Green600, MaterialSkin.Primary.BlueGrey900, MaterialSkin.Primary.BlueGrey500, MaterialSkin.Accent.Orange700, MaterialSkin.TextShade.WHITE);
+        }
+
+        private void Auth_Load(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
